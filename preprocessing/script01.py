313d2025575bb9fdbb4f408c9617d49ae64ceda6
Shivam Gravity <shivamgravity@gmail.com>
2026-03-07 03:13:56 +0530

    updated preprocessing file

--- preprocessing/script01.py
+++ preprocessing/script01.py
@@ -1,6 +1,21 @@
+def loading_data():
+    # code to load data
+
+    pass
+
+def eda():
+    # code for exploratory data analysis
+
+    pass
+
+def preprocessing():
+    # code for data preprocessing
+
+    pass
+
 def main():
     # main code
-    
+
     pass
 
 if __name__ == "__main__":
