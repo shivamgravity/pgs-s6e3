ef1eefc9933e24f31bead6dfa21c04d49fbcc849
Shivam Gravity <shivamgravity@gmail.com>
2026-03-08 05:26:23 +0530

    updated the preprocessing file

--- preprocessing/script01.py
+++ preprocessing/script01.py
@@ -1,20 +1,25 @@
 def loading_data():
+    # import required libraries
+    import pandas as pd
     # code to load data
+    train = pd.read_csv("dataset/train.csv")
+    test = pd.read_csv("dataset/test.csv")
+    return train, test
 
-    pass
-
-def eda():
-    # code for exploratory data analysis
-
-    pass
-
-def preprocessing():
+def preprocessing(train, test):
     # code for data preprocessing
+    print("Train dataset\n",train.columns)
+    print("Test dataset\n",test.columns)
 
     pass
 
 def main():
     # main code
+    # load the dataset
+    train, test = loading_data()
+
+    # preprocess the dataset
+    preprocessing(train, test)
 
     pass
 
